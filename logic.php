09f19d017bad4ae790c257ea157de2c19a53281e
Sarathram07 <55972197+Sarathram07@users.noreply.github.com>
2021-04-30 16:18:31 +0530

    Add files via upload

---
+++ logic.php
@@ -0,0 +1,9 @@
+<?php
+	$jsondata = file_get_contents("https://api.covid19india.org/data.json");
+	$data = json_decode($jsondata, true);
+	
+	foreach($data as $key => $value){
+		$days_count= count($value)-1;
+		$days_count_prev = $days_count-1;
+	}
+?>
