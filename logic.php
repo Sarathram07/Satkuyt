6ebbf9eb6f1816f2ae55b4d7f71f90d144026a2c
Sarathram07 <55972197+Sarathram07@users.noreply.github.com>
2021-04-30 16:55:10 +0530

    Delete logic.php

--- logic.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$jsondata = file_get_contents("https://api.covid19india.org/data.json");
-	$data = json_decode($jsondata, true);
-	
-	foreach($data as $key => $value){
-		$days_count= count($value)-1;
-		$days_count_prev = $days_count-1;
-	}
-?>
